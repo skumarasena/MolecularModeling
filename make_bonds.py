94cfc9f4eb1cf9bd1350600aa0e7e3689100faf0
Samantha Kumarasena <samantha.kumarasena@students.olin.edu>
2013-11-18 20:41:28 -0500

    Changing set_bonds() to set bonds to Atom objects, rather than string representations.

--- make_bonds.py
+++ make_bonds.py
@@ -5,18 +5,18 @@ def set_bonds(t):
     for i in range(1,len(t)):
 
         if type(t[i]) == Atom and type(t[i-1])==Atom:
-            t[i-1].bonds.append(t[i].name)
-            t[i].bonds.append(t[i-1].name)
+            t[i-1].bonds.append(t[i])
+            t[i].bonds.append(t[i-1])
 
         elif type(t[i]) ==Atom and type(t[i-1]) == list:
-            t[i-2].bonds.append(t[i].name)
-            t[i].bonds.append(t[i-2].name)
+            t[i-2].bonds.append(t[i])
+            t[i].bonds.append(t[i-2])
 
         elif type(t[i]) == list and type(t[i-1]) == Atom:
             sub = t[i]
             print sub[0]
-            t[i-1].bonds.append(sub[0].name)
-            sub[0].bonds.append(t[i-1].name)
+            t[i-1].bonds.append(sub[0])
+            sub[0].bonds.append(t[i-1])
             set_bonds(sub)
 
 
