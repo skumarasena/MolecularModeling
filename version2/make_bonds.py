e2db8acf47a387cdd32e53fe8d07ec6949521a7d
Samantha Kumarasena <samantha.kumarasena@students.olin.edu>
2013-11-22 12:36:50 -0500

    Added .blend1 files to .gitignore.

--- version2/make_bonds.py
+++ version2/make_bonds.py
@@ -10,7 +10,7 @@ def set_bonds(t):
     t: nested list of Atom objects
     Returns: None
     """
-    res = copy.deepcopy(t)
+    res = copy.copy(t)
     
     for i in range(1,len(t)):
 
@@ -33,6 +33,39 @@ def set_bonds(t):
             set_bonds(sub)
 
     return flatten(res)
+
+
+# def set_bonds(t):
+#     """Takes a nested list of empty Atom objects, and gives each atom a list of Atom objects to which it is directly bonded.
+
+#     TODO: make this function work for nested lists.
+#     t: nested list of Atom objects
+#     Returns: None
+#     """
+#     res = copy.deepcopy(t)
+    
+#     for i in range(1,len(t)):
+
+#         #if current element and previous element of the list are both part of the same chain, bond both atoms to each other.
+#         if isinstance(t[i], Atom.Atom) and isinstance(t[i-1], Atom.Atom):
+#             res[i-1].bonds.append(t[i])
+#             res[i].bonds.append(t[i-1])
+
+#         #if current element is an Atom and the previous element represents a subchain, bond the current Atom to the Atom before the subchain.
+#         elif isinstance(t[i], Atom.Atom) and type(t[i-1]) == list:
+#             res[i-2].bonds.append(t[i])
+#             res[i].bonds.append(t[i-2])
+
+#         #If current element is a subchain and the previous element is an Atom, bond the first Atom in the subchain to the previous Atom, then evaluate the subchain recursively. 
+#         elif type(t[i]) == list and isinstance(t[i-1], Atom.Atom):
+#             sub = t[i]
+#             print sub[0]
+#             res[i-1].bonds.append(sub[0])
+#             sub[0].bonds.append(t[i-1])
+#             set_bonds(sub)
+
+#     return flatten(res)
+
 
     # #test code: prints bonds of atoms.
     # for atom in t:
