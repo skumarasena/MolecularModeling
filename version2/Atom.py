12da3eadd687d5ab7d2560078e6db51d34baa553
Claire Barnes <claire.barnes@students.olin.edu>
2013-12-10 12:00:46 -0500

    refactoring Atom.py

--- version2/Atom.py
+++ version2/Atom.py
@@ -10,7 +10,11 @@ class Atom(object):
 		bonds: list of Atom objects
 		"""
 		self.name = name
-		self.bonds = bonds
+		if bonds == None:
+			self.bonds = []
+		else: 
+			self.bonds = bonds
+
 		if pos is None:
 			self.pos = [0,0,0]
 		else:
@@ -31,16 +35,9 @@ class Atom(object):
 class Hydrogen(Atom):
 	"""Creates an H atom class with properties used to graph"""
 	def __init__(self,name,bonds=None, pos = None):
-		#try:
-		atom = Atom(name, bonds, pos)
-		#except SyntaxError:
-		#	atom = Atom(name)
-		self.name = atom.name
-		self.pos = atom.pos
-		if atom.bonds == None:
-			self.bonds = []
-		else:
-			self.bonds = atom.bonds
+
+		super(Hydrogen,self).__init__(name,bonds, pos)
+
 		self.radius = 1
 		self.color = (1,1,1) #makes it white in blender
 
@@ -48,13 +45,11 @@ class Hydrogen(Atom):
 class Nitrogen(Atom):
 	"""Creates an N atom class with properties used to graph"""
 	def __init__(self,name,bonds=None, pos=None):
-		atom = Atom(name, bonds, pos)
-		self.name = atom.name
-		self.pos = atom.pos
-		if atom.bonds == None:
-			self.bonds = ['e']
-		else:
-			self.bonds = atom.bonds
+
+		super(Nitrogen,self).__init__(name,bonds)
+
+		
+		self.bonds.append('e')
 		self.color = (135/255,206/255,235/255) #makes it sky blue in blender
 		self.radius = 3
 
@@ -113,33 +108,6 @@ class Sulfur(Atom):
 		self.color = (1,215/255,0) #gold in blender
 
 
- 
-# def makeMaterial(name, diffuse, specular, alpha):
-#     mat = bpy.data.materials.new(name)
-#     mat.diffuse_color = diffuse
-#     mat.diffuse_shader = 'LAMBERT' S
-#     mat.diffuse_intensity = 1.0 
-#     mat.specular_color = specular
-#     mat.specular_shader = 'COOKTORR'
-#     mat.specular_intensity = 0.5
-#     mat.alpha = alpha
-#     mat.ambient = 1
-#     return mat
- 
-# def setMaterial(ob, mat):
-#     me = ob.data
-#     me.materials.append(mat)
- 
-# def make_atom(origin,atom):
-#     atom_mat = makeMaterial(atom.name, atom.color, (0.5,0.5,0), 0.5)
-#     bpy.ops.mesh.primitive_uv_sphere_add(location=origin,size=atom.radius)
-#     bpy.ops.transform.translate(value=(0,0,0))
-#     setMaterial(bpy.context.object, atom_mat)
- 
-# if __name__ == "__main__":
-#     run((0,0,0))
-
-
 def main():
 	#initializing atoms with bonds, and without bonds
 	atom1 = Hydrogen('H1',['bond1'])#, [1,2,3])
@@ -151,30 +119,30 @@ def main():
 	t = [atom1, atom2, atom3]
 
 	#testing...
-	print(atom1)
-	print(atom2)
-	print(atom3)
-	print(t)
+	print atom1
+	print atom2
+	# print(atom3)
+	# print(t)
 
 	#can we append bonds to atom objects that already have a bond list?
-	atom2.bonds.append('bond2.5')
-	print(atom2)
+	# atom2.bonds.append('bond2.5')
+	# print(atom2)
 
 	#can we append bonds to atom objects that do not have a bond list yet?
-	atom3.bonds.append('bond3')
-	print(atom3)
+	# atom3.bonds.append('bond3')
+	# print(atom3)
 
 	#can we append atoms to bond lists?
-	atom1.bonds.append(atom2)
-	print(atom1)
+	# atom1.bonds.append(atom2)
+	# print(atom1)
 
 	#can positions be modified?
-	atom1.pos = [2,3,4]
-	print(atom1)
+	# atom1.pos = [2,3,4]
+	# print(atom1)
 
 	#can position variables be created?
-	atom3.pos = [3,4,5]
-	print(atom3)
+	# atom3.pos = [3,4,5]
+	# print(atom3)
 
 
 
