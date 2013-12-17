6adb0e973f87796b58e7e8b84f8331d8c9500131
Emily <emily.tumang@students.olin.edu>
2013-12-16 20:40:38 -0500

    Commented to_cartesian and filled in docstring.

--- version2/to_cartesian.py
+++ version2/to_cartesian.py
@@ -1,7 +1,14 @@
 import math
 
 def to_cartesian(theta, phi, r):
-    '''Converts spherical or cylindrical coordinates to cartesian ones. Angles in radians.'''
+    '''Converts spherical or cylindrical coordinates to cartesian ones. Angles in radians.
+
+    theta: Angle in the xy plane
+    phi: Angle from z-axis
+    r: radius
+
+    return: tuple representing position in (x,y,z) format
+    '''
 	x = math.cos(theta)*math.sin(phi)*r
     y = math.sin(theta)*math.sin(phi)*r
     z = math.cos(phi)*r
