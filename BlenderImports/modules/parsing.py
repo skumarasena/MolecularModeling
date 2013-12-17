afd71a352c54b91932dada8ed586b546658882de
Claire Barnes <claire.barnes@students.olin.edu>
2013-12-17 12:13:49 -0500

    Update parsing in blenderimports

--- BlenderImports/modules/parsing.py
+++ BlenderImports/modules/parsing.py
@@ -4,11 +4,11 @@ import ast
 
 i = 0
 
-#global bonds = {}
-
 
 def remove_h(formula):
 	"""Takes standard structural chemical formula, and removes all hydrogens from the formula.
+
+	TODO: loops
 
 	formula: string
 	Returns: string
@@ -39,53 +39,106 @@ def make_list(formula):
 	#Splits the string into elements by spaces, replaces parentheses with brackets, then evaluates the resulting expression as a Python expression. The result is a list. 
 	return eval(str(t2.split()).replace("'(',", '[').replace("')'",']'))
 
-def number_list(t):
-	"""Takes a nested list that represents a chemical formula (returned by the function make_list()), makes an analogous list of atoms, and numbers the atoms in order.
+# def make_list(formula):
+# 	"""Takes stripped formula (i.e. a structural formula which has gone through remove_h()) and converts it to a nested list.
 
-	t: list of str
-	Returns: list of Atoms
-	"""	
-	global i
-	res = []
-	for elem in t:
-		if type(elem)==str:
-			#res.append(elem + str(i))		#appends string
-			if elem == 'C':
-				res.append(Atom.Carbon(elem + str(i)))	#appends atom
-			if elem == 'N':
-				res.append(Atom.Nitrogen(elem + str(i)))	#appends atom
-			if elem == 'O':
-				res.append(Atom.Oxygen(elem + str(i)))	#appends atom
-			if elem == 'P':
-				res.append(Atom.Phosphorus(elem + str(i)))	#appends atom	
-			if elem == 'S':
-				res.append(Atom.Sulfur(elem + str(i)))	#appends atom	
-			i +=1
-		if type(elem)==list:
-			res.append(number_list(elem))
-	return res
+# 	TODO: handle formulas with parenthetical multiplicity, ex. C(CH3)3
 
-#DOES NOT WORK: attempted to write number_list without globals.
-# def number_list(t, i):
+# 	formula: string
+# 	Returns: list
+# 	"""
+# 	#replaces parentheses with brackets
+# 	s = formula.replace('(', '[').replace(')', ']')
+	
+# 	#puts spaces between each character of the string
+# 	t = list(s)
+# 	t2 = ''.join([c + ' ' for c in t])
+# 	print t2
+
+# 	print ast.literal_eval(str(t2.split()))
+
+
+
+
+
+
+# def number_list(t):
 # 	"""Takes a nested list that represents a chemical formula (returned by the function make_list()), makes an analogous list of atoms, and numbers the atoms in order.
 
 # 	t: list of str
 # 	Returns: list of Atoms
 # 	"""	
-# #	global i
+# 	global i
 # 	res = []
 # 	for elem in t:
 # 		if type(elem)==str:
-# 			res.append(elem + str(i))		#appends string
-# 			#res.append(Atom(elem, elem + str(i)))	#appends atom
+# 			#res.append(elem + str(i))		#appends string
+# 			if elem == 'C':
+# 				res.append(Atom.Carbon(elem + str(i)))	#appends atom
+# 			if elem == 'N':
+# 				res.append(Atom.Nitrogen(elem + str(i)))	#appends atom
+# 			if elem == 'O':
+# 				res.append(Atom.Oxygen(elem + str(i)))	#appends atom
+# 			if elem == 'P':
+# 				res.append(Atom.Phosphorus(elem + str(i)))	#appends atom	
+# 			if elem == 'S':
+# 				res.append(Atom.Sulfur(elem + str(i)))	#appends atom	
 # 			i +=1
 # 		if type(elem)==list:
-# 			res.append(number_list(elem, i))
+# 			res.append(number_list(elem))
 # 	return res
 
+class Parser(object):
+	"""Traverses a chemical formula, creates analogous atoms, and assigns them names."""
+	def __init__(self):
+		"""Initializes instance of the Parser class."""
+		self.i = 0
+	def __str__(self):
+		return 'Count: %d' % self.i
+
+	def incr(self):
+		"""Increments the counter."""
+		self.i += 1
+
+def parse_list(t, p):
+	"""Takes a nested list that represents a chemical formula (returned by the function make_list()), makes an analogous list of atoms, and numbers the atoms in order.
+
+	t: list of str
+	p: Parser object
+	Returns: list of Atoms
+	"""	
+	res = []
+	for elem in t:
+		if type(elem)==str:
+			#res.append(elem + str(i))		#appends string
+			if elem == 'C':
+				res.append(Atom.Carbon(elem + str(p.i)))	#appends atom
+			if elem == 'N':
+				res.append(Atom.Nitrogen(elem + str(p.i)))	#appends atom
+			if elem == 'O':
+				res.append(Atom.Oxygen(elem + str(p.i)))	#appends atom
+			if elem == 'P':
+				res.append(Atom.Phosphorus(elem + str(p.i)))	#appends atom	
+			if elem == 'S':
+				res.append(Atom.Sulfur(elem + str(p.i)))	#appends atom	
+			p.incr()
+		if type(elem)==list:
+			res.append(parse_list(elem, p))
+	return res
+
+def number_list(t):
+	"""Takes a nested list that represents a chemical formula (returned by the function make_list()), uses parse_list() and a Parser object to number the atoms in order. 
+
+	t: list of str
+	Returns: list of Atoms"""
+	
+	p = Parser()
+	res = parse_list(t, p)
+	return res
 
 
 def main():
+
 	#I attempted to test number_list()
 	i = 0
 	x = number_list(['C', 'C', 'C'])
